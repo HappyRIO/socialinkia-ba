3f8d5cafe7f74607165d4f412278bd3a53439c1b
mernking <davtekdon@gmail.com>
2024-11-09 15:48:40 +0100

    [UPDATE]: fixed build proble

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,13 +3,16 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    headers: {
-      "Cache-Control": "no-store", // Prevents the browser from caching assets
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
     },
   },
-  optimizeDeps: {
-    force: true, // forces Vite to re-optimize dependencies on every server start
-  },
-  plugins: [react()],
 });
