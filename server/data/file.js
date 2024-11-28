ff6c18230bfec5062d7dac8a46ee3c7fc2293694
mernking <davtekdon@gmail.com>
2024-11-28 22:21:01 +0100

    [update]: adding logs to cloudinary

--- server/data/file.js
+++ server/data/file.js
@@ -2,6 +2,7 @@
 const cloudinary = require("cloudinary").v2;
 
 const connectCloudinary = () => {
+  console.log("cloudinary connected");
   cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
