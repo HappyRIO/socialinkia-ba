9c5571b7fbc6ac5c507735415fb44f6a57a729b9
mernking <davtekdon@gmail.com>
2024-11-30 21:52:42 +0100

    [update] ig strin

--- server/routes/apps/Instagram.js
+++ server/routes/apps/Instagram.js
@@ -18,9 +18,18 @@ router.get("/auth/instagram", isSessionValid, async (req, res) => {
     return res.status(500).send("Instagram Client ID or Redirect URI not set.");
   }
 
-  const scope = `instagram_business_basic,instagram_business_content_publish,email,public_profile`;
+  // const scope = `instagram_business_basic,instagram_business_content_publish,email,public_profile`;
+  const scope = [
+    "instagram_basic",
+    // "instagram_business_basic",
+    "instagram_content_publish",
+    "instagram_manage_insights",
+    "pages_show_list",
+    "business_management",
+  ].join(",");
 
-  let instagramAuthUrl = `https://www.instagram.com/oauth/authorize?enable_fb_login=0&force_authentication=1&client_id=${instagramClientId}&redirect_uri=${instagramRedirectUri}&response_type=code&scope=${scope}`;
+  // let instagramAuthUrl = `https://www.instagram.com/oauth/authorize?enable_fb_login=0&force_authentication=1&client_id=${instagramClientId}&redirect_uri=${instagramRedirectUri}&response_type=code&scope=${scope}`;
+  const instagramAuthUrl = `https://www.facebook.com/v17.0/dialog/oauth?client_id=${instagramClientId}&redirect_uri=${instagramRedirectUri}&state=${randomState}&response_type=code&scope=${scope}`;
 
   if (req.user?.instagramAccessToken) {
     // Reauthenticate if user already has a token
